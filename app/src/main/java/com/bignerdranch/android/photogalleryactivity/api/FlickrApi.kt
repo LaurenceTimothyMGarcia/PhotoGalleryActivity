de17b957bdde283c1c89bd27e760ad9c025f887f
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-10-23 22:44:46 -0700

    moving toward the repo

---
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/api/FlickrApi.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.photogalleryactivity.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface FlickrApi {
+    @GET("/")
+    fun fetchContents(): Call<String>
+}
