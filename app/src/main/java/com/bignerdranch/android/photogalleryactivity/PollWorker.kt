b05ce801bd9e89afd9fb5216bf539ef7ead299a3
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2022-10-25 11:01:49 -0700

    page 1112

--- app/src/main/java/com/bignerdranch/android/photogalleryactivity/PollWorker.kt
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/PollWorker.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.photogalleryactivity
 
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -54,8 +55,16 @@ class PollWorker(val context: Context, workerParams: WorkerParameters)
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0, notification)
 
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION))
+
         }
 
         return Result.success()
     }
+
+    companion object {
+        const val ACTION_SHOW_NOTIFICATION =
+            "com.bignerdranch.android.photogallery.SHOW_NOTIFICATION"
+    }
+
 }
