58a5556023a6eeda2f140f0ab9080a943c24dd98
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-10-24 22:46:35 -0700

    pg 1057

---
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/PhotoInterceptor.kt
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.photogalleryactivity
+
+import okhttp3.HttpUrl
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+private const val API_KEY = "6b55caca3a8277313e88b98d55461237"
+
+class PhotoInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+
+        val originalRequest: Request = chain.request()
+        val newUrl: HttpUrl = originalRequest.url().newBuilder()
+            .addQueryParameter("api_key", API_KEY)
+            .addQueryParameter("format", "json")
+            .addQueryParameter("nojsoncallback", "1")
+            .addQueryParameter("extras", "url_s")
+            .addQueryParameter("safesearch", "1")
+            .build()
+        val newRequest: Request = originalRequest.newBuilder()
+            .url(newUrl)
+            .build()
+        return chain.proceed(newRequest)
+    }
+}
--- app/src/main/java/com/bignerdranch/android/photogalleryactivity/api/FlickrApi.kt
+++ app/src/main/java/com/bignerdranch/android/photogalleryactivity/api/FlickrApi.kt
@@ -4,19 +4,18 @@ import com.bignerdranch.android.photogalleryactivity.FlickrResponse
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.Url
 
 interface FlickrApi {
-    @GET(
-        "services/rest/?method=flickr.interestingness.getList" +
-                "&api_key=6b55caca3a8277313e88b98d55461237" +
-                "&format=json" +
-                "&nojsoncallback=1" +
-                "&extras=url_s"
-    )
+
+    @GET("services/rest?method=flickr.interestingness.getList")
     fun fetchPhotos(): Call<FlickrResponse>
 
     @GET
     fun fetchUrlBytes(@Url url: String): Call<ResponseBody>
+
+    @GET("services/rest?method=flickr.photos.search")
+    fun searchPhotos(@Query("text") query: String): Call<FlickrResponse>
 
 }
